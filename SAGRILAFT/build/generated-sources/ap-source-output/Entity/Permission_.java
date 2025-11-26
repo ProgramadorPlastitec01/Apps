7cecf34e1bd31c68a30eec96e21716e27de3e62f
sebast1an2 <Programador.TI2@PLASTITEC.SA>
2025-11-26 09:08:25 -0500

    Sebas - Se suben actualizaciones para sagrilaft

--- SAGRILAFT/build/generated-sources/ap-source-output/Entity/Permission_.java
+++ SAGRILAFT/build/generated-sources/ap-source-output/Entity/Permission_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-11-20T17:24:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-11-26T09:06:46")
 @StaticMetamodel(Permission.class)
 public class Permission_ { 
 
