d2bdc593e3cc0e76e5856b61c4526c73b34f37da
ProgramadorPlastitec01 <prog.ti2924@gmail.com>
2026-07-22 10:31:28 -0500

    Actualización PMP en GIT.

--- PMP MF/build/generated-sources/ap-source-output/Entidades/Rol_.java
+++ PMP MF/build/generated-sources/ap-source-output/Entidades/Rol_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-03-30T08:14:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2026-05-22T08:52:36")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
